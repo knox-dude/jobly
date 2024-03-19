88521fdb636bf19c689bd28937c7152916e296e5
Andrew Knox <andrew.knox.r@gmail.com>
2024-03-19 10:00:36 -0700

    added model logic for companies findall query string feature

--- models/company.js
+++ models/company.js
@@ -18,46 +18,74 @@ class Company {
 
   static async create({ handle, name, description, numEmployees, logoUrl }) {
     const duplicateCheck = await db.query(
-          `SELECT handle
+      `SELECT handle
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]
+    );
 
     if (duplicateCheck.rows[0])
       throw new BadRequestError(`Duplicate company: ${handle}`);
 
     const result = await db.query(
-          `INSERT INTO companies
+      `INSERT INTO companies
            (handle, name, description, num_employees, logo_url)
            VALUES ($1, $2, $3, $4, $5)
            RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`,
-        [
-          handle,
-          name,
-          description,
-          numEmployees,
-          logoUrl,
-        ],
+      [handle, name, description, numEmployees, logoUrl]
     );
     const company = result.rows[0];
 
     return company;
   }
 
+/**
+ * Builds the SQL statement for finding all companies based on the query string parameters.
+ *
+ * @param {Object} queryString - The query string parameters.
+ * @param {string} [queryString.name] - The company name.
+ * @param {number} [queryString.minEmployees] - The minimum number of employees.
+ * @param {number} [queryString.maxEmployees] - The maximum number of employees.
+ * @returns {{sql: string, values: Array}} - The SQL statement and the parameter values.
+ */
+static buildFindAllSQLWithQueryString(queryString) {
+  // build base SQL statement
+  let sql = `SELECT handle,
+      name, 
+      description, 
+      num_employees AS "numEmployees",
+      logo_url AS "logoUrl"
+      FROM companies`;
+  // build sql query using query string parameters
+  let startedSql = false;
+  let paramCount = 1;
+  let values = [];
+  // loop through params of query string, build sql based on what query parameters exist
+  for (let key in queryString) {
+    sql = startedSql ? sql + " AND " : sql + " WHERE ";
+    if (key === "name") {
+      sql = sql + ` name ILIKE '%' || $${paramCount} || '%'`;
+    } else if (key === "minEmployees") {
+      sql = sql + ` num_employees >= $${paramCount}`;
+    } else if (key === "maxEmployees") {
+      sql = sql + ` num_employees <= $${paramCount}`;
+    }
+    startedSql = true;
+    paramCount++;
+    values.push(queryString[key]);
+  }
+  sql = sql + " ORDER BY name";
+  return {sql, values};
+}
+
   /** Find all companies.
    *
    * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
    * */
 
-  static async findAll() {
-    const companiesRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           ORDER BY name`);
+  static async findAll(queryString) {
+    const { sql, values } = Company.buildFindAllSQLWithQueryString(queryString);
+    const companiesRes = await db.query(sql, values);
     return companiesRes.rows;
   }
 
@@ -71,14 +99,15 @@ class Company {
 
   static async get(handle) {
     const companyRes = await db.query(
-          `SELECT handle,
+      `SELECT handle,
                   name,
                   description,
                   num_employees AS "numEmployees",
                   logo_url AS "logoUrl"
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]
+    );
 
     const company = companyRes.rows[0];
 
@@ -100,12 +129,10 @@ class Company {
    */
 
   static async update(handle, data) {
-    const { setCols, values } = sqlForPartialUpdate(
-        data,
-        {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url",
-        });
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      numEmployees: "num_employees",
+      logoUrl: "logo_url",
+    });
     const handleVarIdx = "$" + (values.length + 1);
 
     const querySql = `UPDATE companies 
@@ -131,16 +158,16 @@ class Company {
 
   static async remove(handle) {
     const result = await db.query(
-          `DELETE
+      `DELETE
            FROM companies
            WHERE handle = $1
            RETURNING handle`,
-        [handle]);
+      [handle]
+    );
     const company = result.rows[0];
 
     if (!company) throw new NotFoundError(`No company: ${handle}`);
   }
 }
-
 
 module.exports = Company;
